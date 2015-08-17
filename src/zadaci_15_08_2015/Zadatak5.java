c1c016a39509cc98e03bcbcc7acc3548613e932e
OverLord92 <senjin92@hotmail.com>
2015-08-17 18:22:02 +0200

    commit

--- src/zadaci_15_08_2015/Zadatak5.java
+++ src/zadaci_15_08_2015/Zadatak5.java
@@ -1,46 +1,111 @@
 package zadaci_15_08_2015;
 
+import java.util.ArrayList;
+
 /**
- * *10.26 (Calculator) Revise Listing 7.9, Calculator.java, to accept an
- * expression as a string in which the operands and operator are separated by
- * zero or more spaces. For example, 3+4 and 3 + 4 are acceptable expressions.
+ * (Implement the StringBuilder class) The StringBuilder class is provided in
+ * the Java library. Provide your own implementation for the following methods
+ * (name the new class MyStringBuilder1): public MyStringBuilder1(String s);
+ * public MyStringBuilder1 append(MyStringBuilder1 s); public MyStringBuilder1
+ * append(int i); public int length(); public char charAt(int index); public
+ * MyStringBuilder1 toLowerCase(); public MyStringBuilder1 substring(int begin,
+ * int end); public String toString();
  */
 
 public class Zadatak5 {
 	/** Main method */
 	public static void main(String[] args) {
-		// Check number of strings passed
-		if (args.length != 3) {
-			System.out
-					.println("Usage: java Calculator operand1 operator operand2");
-			System.exit(0);
-		}
+		
+		//testiranjem metoda
+		
+		MyStringBuilder1 str1 = new MyStringBuilder1("STRING");
+		System.out.println("sustring(1,4) stringa STRING je " + str1.substring(1, 4));
+		
+		MyStringBuilder1 str2 = new MyStringBuilder1("drugi string");
+		str2.append(str1);
+		System.out.println("Dodavanjem sadrzaja str1 u str2 dobijamo: " + str2);
+		
+		str2.append(200);
+		System.out.println("Poslije dodavanje broja 100 u str2 dobijamo: " + str2);
+		
+		System.out.println("Duzina str2 = " + str2.length());
+		
+		MyStringBuilder1 str3 = str1.toLowerCase();
+		System.out.println("str1.toLowerCase = " + str3);
+		
+		System.out.println("Karakter na poziciji 2 u str3 je " + str3.charAt(2));
 
-		// da bi program radio i sa vise spejsova potrebno je samo trimovati
-		// clanove niza args
-		for (int i = 0; i < args.length; i++) {
-			args[i] = args[i].trim();
-		}
-
-		// The result of the operation
-		int result = 0;
-
-		// Determine the operator
-		switch (args[1].charAt(0)) {
-		case '+':
-			result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
-			break;
-		case '-':
-			result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
-			break;
-		case '.':
-			result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
-			break;
-		case '/':
-			result = Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
-		}
-		// Display result
-		System.out.println(args[0] + ' ' + args[1] + ' ' + args[2] + " = "
-				+ result);
 	}
+}
+
+class MyStringBuilder1 {
+	//arraylist u koji mozemo dodavati karaktere
+	ArrayList<Character> chars = new ArrayList<>();
+
+	public MyStringBuilder1() {
+
+	}
+
+	/** Konstruktor koji od stringa pravi stringbuilder */
+	public MyStringBuilder1(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			chars.add(s.charAt(i));
+		}
+	}
+
+	/** Metoda koja na stringbuilder dodaje sadrzaj drugog string buildera */
+	public MyStringBuilder1 append(MyStringBuilder1 s) {
+
+		for (int i = 0; i < s.length(); i++) {
+			this.chars.add(s.chars.get(i));
+		}
+		return this;
+	}
+
+	/** Metoda koja na stringbuilder dodaje integer */
+	public MyStringBuilder1 append(int i) {
+		MyStringBuilder1 appendInt = new MyStringBuilder1(i + "");
+		this.append(appendInt);
+		return this;
+	}
+
+	/** Metoda koja vraca duzinu stringbuildera */
+	public int length() {
+		return chars.size();
+	}
+
+	/** Metoda koja vraca karakter na odredjenoj poziciji */
+	public char charAt(int index) {
+		return this.chars.get(index);
+	}
+
+	/** Metoda koja sva slova u stringbuilderu prebacuje u lower case */
+	public MyStringBuilder1 toLowerCase() {
+
+		for (int i = 0; i < this.length(); i++) {
+			this.chars.set(i, Character.toLowerCase(this.chars.get(i)));
+		}
+
+		return this;
+
+	}
+
+	/** Metoda koja vraca substring string buildera */
+	public MyStringBuilder1 substring(int begin, int end) {
+		MyStringBuilder1 result = new MyStringBuilder1();
+		for(int i = begin; i < end; i++){
+			result.chars.add(this.chars.get(i));
+		}
+		
+		return result;
+	}
+	
+	/** Overrajdovana toString metoda */
+	public String toString(){
+		String result = "";
+		for(int i = 0; i < this.length(); i++)
+			result += this.chars.get(i);
+		return result;
+	}
+
 }
